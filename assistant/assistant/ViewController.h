9cd00f3c07dc4eb31d678e3f85dee9de7fc91bba
XiaoChen Yung <18730222889@qq.com>
2015-10-01 19:05:36 +0800

    项目初始化

---
+++ assistant/assistant/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  assistant
+//
+//  Created by 杨晓晨 on 15/10/1.
+//  Copyright (c) 2015年 yangxiaochen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ assistant/assistant/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  assistant
+//
+//  Created by 杨晓晨 on 15/10/1.
+//  Copyright (c) 2015年 yangxiaochen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
